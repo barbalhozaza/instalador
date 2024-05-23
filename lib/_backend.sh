693514f5caa6951fefdf2063c4e6db126fec795a
Vitor-Veiga-Dias <vitorveigadias@gmail.com>
2024-05-23 12:50:00 -0300

    fix...

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -13,19 +13,18 @@ backend_redis_create() {
 
   sleep 2
 
-sudo su - root <<EOF
-sudo useradd deploy
-sudo usermod -aG sudo deploy
-docker run --name redis-${instancia_add} -p ${redis_port}:6379 --restart always --detach redis redis-server --requirepass ${mysql_root_password}
+  sudo su - root <<EOF
+  usermod -aG docker deploy
+  docker run --name redis-${instancia_add} -p ${redis_port}:6379 --restart always --detach redis redis-server --requirepass ${mysql_root_password}
   
-sleep 2
-sudo su - postgres
-createdb ${instancia_add};
-psql
-CREATE USER ${instancia_add} SUPERUSER INHERIT CREATEDB CREATEROLE;
-ALTER USER ${instancia_add} PASSWORD '${mysql_root_password}';
-\q
-exit
+  sleep 2
+  sudo su - postgres
+  createdb ${instancia_add};
+  psql
+  CREATE USER ${instancia_add} SUPERUSER INHERIT CREATEDB CREATEROLE;
+  ALTER USER ${instancia_add} PASSWORD '${mysql_root_password}';
+  \q
+  exit
 EOF
 
 sleep 2
@@ -55,7 +54,6 @@ backend_set_env() {
   frontend_url=https://$frontend_url
 
 sudo su - deploy << EOF
-  mkdir -p /home/deploy/${instancia_add}/backend
   cat <<[-]EOF > /home/deploy/${instancia_add}/backend/.env
 NODE_ENV=
 BACKEND_URL=${backend_url}
